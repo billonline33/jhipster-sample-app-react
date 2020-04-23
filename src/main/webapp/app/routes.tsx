a22dbf47865e1a4bcff01b4bf0db670d71699cb1
Bill Huang <billonline33@hotmail.com>
2020-04-23 19:27:42 +1000

    WIP

--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Loadable from 'react-loadable';
 
 import Login from 'app/modules/login/login';
@@ -14,6 +14,8 @@ import PrivateRoute from 'app/shared/auth/private-route';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import PageNotFound from 'app/shared/error/page-not-found';
 import { AUTHORITIES } from 'app/config/constants';
+import UnderConstruction from './modules/shared/underConstruction';
+import UnderConstruction2 from './modules/shared/underConstruction2';
 
 const Account = Loadable({
   loader: () => import(/* webpackChunkName: "account" */ 'app/modules/account'),
@@ -28,6 +30,8 @@ const Admin = Loadable({
 const Routes = () => (
   <div className="view-routes">
     <Switch>
+       <Route path="/underConstruction" component={UnderConstruction} />
+      <Route path="/underConstruction2" component={UnderConstruction2} />
       <ErrorBoundaryRoute path="/login" component={Login} />
       <ErrorBoundaryRoute path="/logout" component={Logout} />
       <ErrorBoundaryRoute path="/account/register" component={Register} />
